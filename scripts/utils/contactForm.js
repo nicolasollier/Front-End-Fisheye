ff3f53a6617041bff9825142cf8914cd4dce2abf
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-03-29 14:48:28 +0200

    adds form validations

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -5,6 +5,11 @@ import { getPhotographer } from "../../scripts/pages/api.js";
 let params = new URLSearchParams(window.location.search);
 let id = parseInt(params.get("id"));
 
+// Get DOM elements
+const firstNameInput = document.querySelector("#firstName");
+const lastNameInput = document.querySelector("#lastName");
+const emailInput = document.querySelector("#email");
+
 const openContactModalButton = document.querySelector(".contact_button");
 const closeContactModalButton = document.querySelector(".contact_modal__close");
 const modal = document.getElementById("contact_modal");
@@ -22,14 +27,95 @@ closeContactModalButton.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
+// REGEX
+const regexName = /^[a-zA-ZÀ-ÿ\s]{1,}$/;
+const regexEmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+
+// Validates inputs
+let firstNameIsValid = false;
+let lastNameIsValid = false;
+let emailIsValid = false;
+
+
+firstNameInput.addEventListener("input", () => {
+    let isValid = regexName.test(firstNameInput.value) && firstNameInput.value.length > 0;
+    if (isValid) {
+        document.querySelector(".first-name__feedback").style.display = "none";
+        firstNameInput.classList.remove("invalid");
+        firstNameInput.classList.add("valid");
+
+        return firstNameIsValid = true;
+    }
+    if (!isValid) {
+        firstNameInput.classList.remove("valid");
+        firstNameInput.classList.add("invalid");
+        document.querySelector(".first-name__feedback").style.display = "block";
+
+        return firstNameIsValid = false;
+    }
+});
+
+lastNameInput.addEventListener("input", () => {
+    let isValid = regexName.test(lastNameInput.value) && lastNameInput.value.length > 0;
+    if (isValid) {
+        document.querySelector(".last-name__feedback").style.display = "none";
+        lastNameInput.classList.remove("invalid");
+        lastNameInput.classList.add("valid");
+
+        return lastNameIsValid = true;
+    }
+    if (!isValid) {
+        lastNameInput.classList.remove("valid");
+        lastNameInput.classList.add("invalid");
+        document.querySelector(".last-name__feedback").style.display = "block";
+
+        return lastNameIsValid = false;
+    }
+});
+
+emailInput.addEventListener("input", () => {
+    let isValid = regexEmail.test(emailInput.value) && emailInput.value.length > 0;
+    if (isValid) {
+        document.querySelector(".email__feedback").style.display = "none";
+        emailInput.classList.remove("invalid");
+        emailInput.classList.add("valid");
+
+        return emailIsValid = true;
+    }
+    if (!isValid) {
+        emailInput.classList.remove("valid");
+        emailInput.classList.add("invalid");
+        document.querySelector(".email__feedback").style.display = "block";
+
+        return emailIsValid = false;
+    }
+});
+
+// Reset form
+const resetForm = () => {
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    document.querySelector("#message").value = "";
+};
+
 contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log({
-        firstName: e.target.firstName.value,
-        lastName: e.target.lastName.value,
-        email: e.target.email.value,
-        message: e.target.message.value,
-    });
-    e.target.reset();
-    modal.style.display = "none";
+    if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+        const firstName = firstNameInput.value;
+        const lastName = lastNameInput.value;
+        const email = emailInput.value;
+        const message = document.querySelector("#message").value;
+
+        const contact = {
+            firstName,
+            lastName,
+            email,
+            message,
+        };
+
+        console.log(contact);
+        modal.style.display = "none";
+        resetForm();
+    }
 });
